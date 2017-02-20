eb2615b0ab908ad78368252987f1dc788d9dcc4a
root <contact@mohamedmooga.com>
2017-02-20 14:21:05 -0500

    init

---
+++ Main.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Mohamed Badawi
+ * Delivery: prof. Renz HAW-Hamburg
+ * */
+import java.util.*;
+public class Main {
+	public static void main(String [] args){
+//			System.out.print("Mooga");
+	String  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int n = alpha.length();
+	
+	String result = new String();
+	Random r = new Random();
+	String [] gen = new String[100];
+	for(int j =0;j< gen.length;j++){
+		result= "";
+		for(int i=0; i< 10;i++){
+			result += alpha.charAt(r.nextInt(n));
+		}
+		gen[j] = result;
+	}
+	
+	for(int k=0;k<10;k++){
+		System.out.print(gen[k]+", ");
+	}
+	}
+}
